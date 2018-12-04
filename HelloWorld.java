61328103d7af23cdc78e21ec732bbead5ff107cf
Owen <45383826+auniqb@users.noreply.github.com>
2018-12-04 23:57:19 +1100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+This is a simple Java program.
+Call this file "helloworld.java".
+*/
+    class HelloWorld 
+         {
+// Your program begins with a call to main().
+
+                    public static void main(String args[]) 
+                  {
+                   System.out.println("This is a simple Java program.");
+                   }
+          }
+		  
+		 
